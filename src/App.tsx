5083eefc3bd696041a39b2b2bcbcf4f0bc6bedec
Android789515 <derflug@mailfence.com>
2024-10-16 09:10:26 +0900

    feat: render the app ui
    Signed-off-by: Android789515 <derflug@mailfence.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
 import styles from './App.module.css';
 
+import { Layout } from 'components/layout';
+import { Header } from 'components/header';
+import { Title } from 'components/title';
+import { DownloadButton } from 'features/download-button';
+import { Workspace } from 'features/workspace';
+
 export const App = () => {
    return (
       <div
          className={styles.app}
-      ></div>
+      >
+         <Layout>
+            <Header>
+               <Title />
+
+               <DownloadButton />
+            </Header>
+
+            <Workspace />
+         </Layout>
+      </div>
    );
 };
