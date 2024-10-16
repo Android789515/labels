8caf07baf65ba52ed71a94ce4c6985a30de5ff8b
Android789515 <derflug@mailfence.com>
2024-10-16 09:44:09 +0900

    fix: unoverridable default styles
    Signed-off-by: Android789515 <derflug@mailfence.com>

--- src/components/icon-button/IconButton.tsx
+++ src/components/icon-button/IconButton.tsx
@@ -1,21 +1,29 @@
 import { type HTMLAttributes } from 'react';
 
+import { type ClassName } from 'types';
+
 import { Button } from 'components/button';
 
 interface Props extends HTMLAttributes<HTMLButtonElement> {
    icon: string;
    description: string;
+   customStyles?: {
+      button: ClassName;
+      icon: ClassName;
+   };
    iconProps?: HTMLAttributes<HTMLImageElement>;
 }
 
-export const IconButton = ({ icon, description, iconProps, ...rest }: Props) => {
+export const IconButton = ({ icon, description, customStyles, iconProps, ...rest }: Props) => {
    return (
       <Button
+         customStyles={customStyles?.button}
          {...rest}
       >
          <img
             src={icon}
             alt={description}
+            className={customStyles?.icon}
             {...iconProps}
          />
       </Button>
