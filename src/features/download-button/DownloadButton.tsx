ddb61ec2b79ebdb218559420e55c6f766d401f05
Android789515 <derflug@mailfence.com>
2024-10-16 09:09:51 +0900

    feat: download button boilerplate
    Signed-off-by: Android789515 <derflug@mailfence.com>

---
+++ src/features/download-button/DownloadButton.tsx
@@ -0,0 +1,11 @@
+import styles from './DownloadButton.module.css';
+
+import { Button } from 'components/button';
+
+export const DownloadButton = () => {
+   return (
+      <Button
+         className={styles.downloadButton}
+      ></Button>
+   );
+};
