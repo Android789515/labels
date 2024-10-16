322dbc29ae02bb1567a37e11bedd5d108175162a
Android789515 <derflug@mailfence.com>
2024-10-16 09:11:54 +0900

    feat: button style defaults
    Signed-off-by: Android789515 <derflug@mailfence.com>

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -5,6 +5,12 @@ type Props = HTMLAttributes<HTMLButtonElement>;
 export const Button = (props: Props) => {
    return (
       <button
+         style={{
+            background: 'none',
+            padding: 0,
+            border: 'none',
+            borderRadius: '.125em',
+         }}
          {...props}
       ></button>
    );
