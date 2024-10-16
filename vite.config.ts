b5555bdb2014015447225d04fc505a51fba12922
Android789515 <derflug@mailfence.com>
2024-10-16 09:08:22 +0900

    feat: css classname type
    Signed-off-by: Android789515 <derflug@mailfence.com>

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
    plugins: [ react() ],
    resolve: {
       alias: {
+         'types': resolve('./src/types.ts'),
          'components': resolve('./src/components'),
       },
    },
